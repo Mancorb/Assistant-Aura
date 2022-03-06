38d3bef31cc5ef96cbcabab9d7a2dec8cf618e7d
Mancorb <andres.m.bosques@gmail.com>
2022-03-05 19:32:55 -0600

    Small modifications
    small modification to the original code from here the commented parts will be discarted

--- Trainer.py
+++ Trainer.py
@@ -5,13 +5,12 @@ import numpy as np
 #Cargar la librería para detectar caras
 #REGRESA: caras detectadas (posicion) y la imagen en gris
 def faceDetection(test_img):
-    #delete all color in am image
-    grey_img = cvtColor(test_img, cv2.COLOR_BGR2GRAY)
-    #Cargar el algoritmo para detectar rostros de cv2
-    face_haar_cascade=cv2.CascadeClassifier('haarCascade/haarcascade_frontalface_default.xml')
-    #Proceso para detectar rostros con múltiples capas de neuronas a cierta escala
-    faces = face_haar_cascade.detectMultiScale(grey_img,scaleFactor=1.32,minNeighbors=10)
-    return faces, grey_img
+    gray_img=cv2.cvtColor(test_img,cv2.COLOR_BGR2GRAY)#convert color image to grayscale
+    face_haar_cascade=cv2.CascadeClassifier('haarCascade\haarcascade_frontalface_default.xml')#Load haar classifier
+    faces=face_haar_cascade.detectMultiScale(gray_img,scaleFactor=1.32,minNeighbors=5)#detectMultiScale returns rectangles
+
+    return faces,gray_img
+
 #Entrenar la red neuronal para detectar un rostro
 def labels_training(directory):
     #Consjunto de caras
@@ -29,8 +28,8 @@ def labels_training(directory):
             id=os.path.basename(path)#clasificar las imagnes de la carpeta con el nombre de la carpeta
             img_path= os.path.join(path,filename)
             #Imprimir la ubicación de la imagen junto con la ubicación
-            print("img_path: ",img_path)
-            print("id: ",id)
+            """ print("img_path: ",img_path)
+            print("id: ",id) """
             #Cargar la imagen de la ubicación
             test_img=cv2.imread(img_path)
             #mostrar una alerta si no se pudo cargar la imagen
@@ -54,7 +53,7 @@ def labels_training(directory):
 
 #Asosiar las imagenes con el ID proporcionado y entrenar a la red neuronal con la librería 
 def train_clasifier(faces,faceID):
-    face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+    face_recognizer = cv2.face.LBPHFaceRecognizer_create()#IF it doesn't owrk use "pip install opencv-contrib-python"
     face_recognizer.train(faces,np.array(faceID))
     return face_recognizer
 #------------------------------------------------------
@@ -63,9 +62,10 @@ def train_clasifier(faces,faceID):
 #Dibujar un rectangulo en la imagen que se esta analizando
 def draw_rect(test_img,face):
     (x,y,w,h)=face
-    cv2.rectangle(test_img,(x,y),(x+w,y+h),(255,0,0),thickness=5)
+    cv2.rectangle(test_img,(x,y),(x+w,y+h),(255,255,255) ,thickness=1)
 
 #escribir un texto  encima de la imagen con el ID de la persona si es que la identifica
-def put_text(test_img,text,x,y):
-    cv2.putText(test_img,text,(x,y),cv2.FONT_HERSHEY_DUPLEX,5,(255,0,0),6)
+def put_text(test_img,user,x,y, confidence):
+    text= f"User:{user},{confidence}"
+    cv2.putText(test_img,text,(x-10,y),cv2.FONT_HERSHEY_PLAIN,1,(255,255,255),1)
 #------------------------------------------------------
--- test2.py
+++ test2.py
@@ -1,25 +1,28 @@
 import cv2
 import os
 import numpy as np
-import Trainer as fr
+import Trainer as tr
 import takePhoto as tP
 
-location=tP.take_picture("subject",None, None,)
+location=tP.take_picture("test",None, None,"images")
 
-test_img =cv2.imread('images/c1.png') 
-faces_detected, grey_img = fr.faceDetection(test_img)
-print(f"faces_detected:{faces_detected}")
+test_img =cv2.imread("images/test.png") 
+faces_detected, grey_img = tr.faceDetection(test_img)
+#print(f"faces_detected:{faces_detected}")
+
+if not len(faces_detected):
+    print("No image detected")
+    exit()
 
 """ for (x,y,w,h) in faces_detected:
-    cv2.rectangle(test_img,(x,y),(x+w,y+h),(255,0,0),thickness=5)
+    cv2.rectangle(test_img,(x,y),(x+w,y+h),(240,240,255),thickness=1)
 
-resized_img = cv2.resize(test_img,(1000,700))
+resized_img = cv2.resize(test_img,(900,800))
 cv2.imshow("face detection res", resized_img)
 cv2.waitKey(0)
 cv2.destroyAllWindows() """
-
-faces,faceID=fr.labels_training('images/0')
-face_rec=fr.train_clasifier(faces,faceID)
+faces,faceID=tr.labels_training('images/0')
+face_rec=tr.train_clasifier(faces,faceID)
 name={0:"master"}
 
 for faces in faces_detected:
@@ -28,12 +31,14 @@ for faces in faces_detected:
     label,confidence=face_rec.predict(roi_grey)
     print(f"confidence:{confidence}")
     print(f"label:{label}")
-    fr.draw_rect(test_img,faces)
+    
     predicted_name=name[label]
-    fr.put_text(test_img,predicted_name,x,y)
 
-resized_img = cv2.resize(test_img,(1000,700))
+    """ tr.draw_rect(test_img,faces)
+    tr.put_text(test_img,predicted_name,x,y,confidence) """
+
+""" resized_img = cv2.resize(test_img,(800,800))
 cv2.imshow("face detection res", resized_img)
 cv2.waitKey(0)
-cv2.destroyAllWindows()
+cv2.destroyAllWindows() """
 
